64a2f7aa9dff1ce124a361c2a99dc95e47adbbd2
csp <1187814176@qq.com>
2019-07-04 00:16:08 +0800

    HystrixDashboard断路器查看器

--- feign/src/main/java/com/cloud/feign/FeignApplication.java
+++ feign/src/main/java/com/cloud/feign/FeignApplication.java
@@ -2,12 +2,16 @@ package com.cloud.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class FeignApplication {
 
     public static void main(String[] args) {
--- feign/src/main/java/com/cloud/feign/controller/WebController.java
+++ feign/src/main/java/com/cloud/feign/controller/WebController.java
@@ -2,9 +2,12 @@ package com.cloud.feign.controller;
 
 import com.cloud.feign.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class WebController {
